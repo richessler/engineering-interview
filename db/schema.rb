b387d5689cf7420ed87e2e9b4c8f6044c030b02f
Richard Hessler <richessler@gmail.com>
2016-02-17 20:50:19 -0500

    Final Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217233911) do
+ActiveRecord::Schema.define(version: 20160218013143) do
 
   create_table "tweets", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at"
+    t.datetime "created_at", null: false
     t.integer  "user_id"
+    t.string   "name",       null: false
   end
 
   create_table "users", force: :cascade do |t|
