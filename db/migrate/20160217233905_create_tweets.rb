03b66fdd24b6e3ff8f8b855097e9bbdb62558432
Richard Hessler <richessler@gmail.com>
2016-02-17 20:48:53 -0500

    additional migrations

--- db/migrate/20160217233905_create_tweets.rb
+++ db/migrate/20160217233905_create_tweets.rb
@@ -1,6 +1,7 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
+      t.string      :name
       t.text        :content
       t.datetime    :created_at
 
