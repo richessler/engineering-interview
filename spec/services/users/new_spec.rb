9ffc1a1ef010691cf3c55f6ab927f8d4910fbaaa
Richard Hessler <richessler@gmail.com>
2016-02-17 20:50:35 -0500

    Added Service and specs

--- spec/services/users/new_spec.rb
+++ spec/services/users/new_spec.rb
@@ -1,5 +1,12 @@
 RSpec.describe Users::New do
-  it "should create a new user" do
+  it 'creates a new user' do
+    before_count = User.count
+    described_class.call({
+                             handle: 'richessler',
+                             email: 'richessler@gmail.com',
+                             encrypted_password: 'i_hope_this_was_good_enough_to_join_the_team_^_^'
+                         })
 
+    expect(User.count).to eq(before_count + 1)
   end
 end
