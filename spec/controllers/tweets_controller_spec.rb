f86f0e68d3b631289584b0b9608c59c707f4e9dc
Richard Hessler <richessler@gmail.com>
2016-02-17 20:54:15 -0500

    Complete tests and controller to get successfully pass tests

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -1,31 +1,54 @@
 RSpec.describe TweetsController do
+  before(:all) do
+    @tweet = Tweet.create(name: 'Richard Hessler', content: '140 characters is never enough so I\'m going to make this more because I didn\'t add validations for length of a "tweet" #herpderpnerd :D')
+  end
   context 'listing tweets' do
-    it 'should assign tweets' do
+    before(:each) do
+      get :index
     end
-    it 'should render a 200 status code' do
+
+    it 'assigns tweets' do
+      expect(assigns(:tweets)).to be_present
     end
-    it 'should render the index template' do
+
+    it 'rendera a 200 status code' do
+      expect(response).to be_success
+    end
+
+    it 'rendera the index template' do
+      expect(response).to render_template(:index)
     end
   end
 
   context 'show a tweet' do
-    it 'should assign tweet' do
+    before(:each) do
+      get :show, id: @tweet.id
     end
-    it 'should render a 200 status code' do
+
+    it 'assigns tweet' do
+      expect(assigns(:tweet)).to be_present
     end
-    it 'should render the show template' do
+    it 'renders a 200 status code' do
+      expect(response).to be_success
+    end
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
     end
 
   end
 
   context 'create a tweet' do
-    it 'should assign tweet' do
+    before(:each) do
+      get :create, { tweet: { name: 'Another Richard', content: 'I follow tweet rules' } }
     end
-    it 'should redirect to tweets index' do
+    it 'assigns tweet' do
+      expect(assigns(:tweet)).to be_present
     end
-    it 'should render a 302 status code' do
+    it 'redirects to tweets index' do
+      expect(response).to redirect_to(:tweets)
     end
-
+    it 'renders a 302 status code' do
+      expect(response.status).to be(302)
+    end
   end
-
 end
